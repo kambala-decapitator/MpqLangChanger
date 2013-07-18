7f47dded5312902b79315e21cf8e77fe788f9449
kambala <decapitator@ukr.net>
2013-07-18 20:43:39 +0300

    added StormLib submodule;
    added MSVS 2012 solution.

--- MpqLangChanger/main.cpp
+++ MpqLangChanger/main.cpp
@@ -10,18 +10,31 @@
 
 #include <iostream>
 
+#define USE_FILE_MPQ_PATH "data\\local\\use"
+
+#ifdef WIN32
+#define MPQ_TEST_PATH L"d:\\Games\\Setup\\Windows\\Diablo 2\\Моды\\Median XL\\тест\\Patch_D2.mpq"
+#else
+#define MPQ_TEST_PATH "/Users/kambala/Downloads/2012 005 rus only"
+#endif
+
 using std::cout;
 using std::endl;
+
+void print_usage()
+{
+
+}
 
 int main (int argc, const char * argv[])
 {
     HANDLE phMPQ = 0;
     DWORD flags = BASE_PROVIDER_FILE | STREAM_PROVIDER_LINEAR | STREAM_FLAG_WRITE_SHARE;
-    bool b = SFileOpenArchive("/Users/kambala/Downloads/2012 005 rus only", 0, flags, &phMPQ);
+    bool b = SFileOpenArchive(MPQ_TEST_PATH, 0, flags, &phMPQ);
     cout << "open result " << b << ", handle " << phMPQ << ", is readonly " << (((TMPQArchive *)phMPQ)->dwFlags & MPQ_FLAG_READ_ONLY) << endl;
 
     HANDLE phUseFile = NULL;
-    b = SFileOpenFileEx(phMPQ, "data\\local\\use", SFILE_OPEN_FROM_MPQ, &phUseFile);
+    b = SFileOpenFileEx(phMPQ, USE_FILE_MPQ_PATH, SFILE_OPEN_FROM_MPQ, &phUseFile);
     cout << "open result " << b << ", handle " << phUseFile << endl;
     char buf;
     DWORD bytes;
@@ -31,17 +44,19 @@ int main (int argc, const char * argv[])
     cout << "close result " << b << endl;
 
     HANDLE phNewUseFile = 0;
-    b = SFileCreateFile(phMPQ, "data\\local\\use", 0, 1, 0, MPQ_FILE_REPLACEEXISTING, &phNewUseFile);
+    b = SFileCreateFile(phMPQ, USE_FILE_MPQ_PATH, 0, 1, 0, MPQ_FILE_REPLACEEXISTING, &phNewUseFile);
     cout << "create result " << b << ", handle " << phNewUseFile << endl;
-    char newLocale = 11;
+    char newLocale = buf ? 0 : 11; // interchange english with russian
     b = SFileWriteFile(phNewUseFile, &newLocale, 1, 0);
-    cout << "write result " << b << endl;
+    cout << "write result " << b << " new locale " << (short)newLocale << endl;
     if (phNewUseFile)
     {
         b = SFileFinishFile(phNewUseFile);
         cout << "finish result " << b << endl;
     }
 
+    b = SFileCompactArchive(phMPQ, 0, false);
+    cout << "compact result " << b << ", error " << GetLastError() << endl;
     b = SFileCloseArchive(phMPQ);
     cout << "close result " << b << endl;
     
