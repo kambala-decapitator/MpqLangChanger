2ea527472bbda7c691b524a94d8a474b89b81a60
kambala <decapitator@ukr.net>
2013-07-19 03:36:03 +0300

    added language table
    added test code to get mpq path from user input

--- MpqLangChanger/main.cpp
+++ MpqLangChanger/main.cpp
@@ -13,13 +13,29 @@
 using std::cout;
 using std::endl;
 
-#define USE_FILE_MPQ_PATH "data\\local\\use"
-
 #ifdef WIN32
 #define MPQ_TEST_PATH L"d:\\Games\\Installed\\Diablo II\\Patch_D2.mpq"
 #else
 #define MPQ_TEST_PATH "/Users/kambala/Downloads/2012 005 rus only"
 #endif
+
+const int kLanguagesCount = 12;
+const char *const kLanguages[kLanguagesCount] = {
+    "ENG (English)",    // 0x00
+    "ESP (Spanish)",    // 0x01
+    "DEU (German)",     // 0x02
+    "FRA (French)",     // 0x03
+    "POR (Portuguese)", // 0x04
+    "ITA (Italian)",    // 0x05
+    "JPN (Japanese)",   // 0x06
+    "KOR (Korean)",     // 0x07
+    "SIN",              // 0x08
+    "CHI (Chinese)",    // 0x09
+    "POL (Polish)",     // 0x0A
+    "RUS (Russian)"     // 0x0B
+};
+
+const char *const kUseFileMpqPath = "data\\local\\use";
 
 
 void print_usage()
@@ -27,19 +43,27 @@ void print_usage()
 
 }
 
-int main (int argc, const char * argv[])
+int main (int argc, const char *argv[])
 {
+    TCHAR mpqPath[MAX_PATH];
+    cout << "enter mpq path: ";
+    std::wcin.getline(mpqPath, MAX_PATH);
+    std::wcout << "entered path: " << mpqPath << endl;
+
     HANDLE phMPQ = 0;
     DWORD flags = BASE_PROVIDER_FILE | STREAM_PROVIDER_LINEAR | STREAM_FLAG_WRITE_SHARE;
-    bool b = SFileOpenArchive(MPQ_TEST_PATH, 0, flags, &phMPQ);
+    bool b = SFileOpenArchive(mpqPath, 0, flags, &phMPQ);
     cout << "open result " << b << ", handle " << phMPQ;
     if (phMPQ)
         cout << ", is readonly " << (((TMPQArchive *)phMPQ)->dwFlags & MPQ_FLAG_READ_ONLY) << endl;
     else
+    {
+        cout << ", error " << GetLastError() << endl;
         return 1;
+    }
 
     HANDLE phUseFile = NULL;
-    b = SFileOpenFileEx(phMPQ, USE_FILE_MPQ_PATH, SFILE_OPEN_FROM_MPQ, &phUseFile);
+    b = SFileOpenFileEx(phMPQ, kUseFileMpqPath, SFILE_OPEN_FROM_MPQ, &phUseFile);
     cout << "open result " << b << ", handle " << phUseFile << endl;
     char buf;
     DWORD bytes;
@@ -49,7 +73,7 @@ int main (int argc, const char * argv[])
     cout << "close result " << b << endl;
 
     HANDLE phNewUseFile = 0;
-    b = SFileCreateFile(phMPQ, USE_FILE_MPQ_PATH, 0, 1, 0, MPQ_FILE_REPLACEEXISTING, &phNewUseFile);
+    b = SFileCreateFile(phMPQ, kUseFileMpqPath, 0, 1, 0, MPQ_FILE_REPLACEEXISTING, &phNewUseFile);
     cout << "create result " << b << ", handle " << phNewUseFile << endl;
     char newLocale = buf ? 0 : 11; // interchange english with russian
     b = SFileWriteFile(phNewUseFile, &newLocale, 1, 0);
